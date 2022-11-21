519a8868a94ec189078faf105277216856edad08
OleksandrS06 <112249299+OleksandrS06@users.noreply.github.com>
2022-11-21 23:03:32 +0100

    const delay renamed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,19 +8,19 @@ console.dir(formEl.elements);
 
 const onCreatePromisesBtnClick = event => {
   event.preventDefault();
-  delay = Number(formEl.elements.delay.value);
+  firstDelay = Number(formEl.elements.delay.value);
   step = Number(formEl.elements.step.value)
   amount = Number(formEl.elements.amount.value);
-  console.log(delay, amount, step);
+  // console.log(delay, amount, step);
   
   for (let i = 1; i <= amount; i += 1){
-    createPromise( i, delay).then(({ position, delay }) => {
+    createPromise( i, firstDelay).then(({ position, delay }) => {
     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
-    delay += step;
+    firstDelay += step;
   }
   
 }
